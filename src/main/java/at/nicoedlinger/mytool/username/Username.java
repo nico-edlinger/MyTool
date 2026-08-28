93fc637dbf88d38c29ac25525398c05e6c090077
nico-edlinger <edlingernico2008@gmail.com>
2026-08-28 11:43:26 +0200

    added style to Username window

--- src/main/java/at/nicoedlinger/mytool/username/Username.java
+++ src/main/java/at/nicoedlinger/mytool/username/Username.java
@@ -1,12 +1,18 @@
 package at.nicoedlinger.mytool.username;
 
 import at.nicoedlinger.mytool.logger.Logger;
+import at.nicoedlinger.mytool.ui.GUI;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Control;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 import java.io.BufferedReader;
@@ -17,18 +23,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Username {
-    /**
-     * The path to the file where the username is saved
-     */
-    private static final String PATH = "src\\main\\resources\\username.txt";
-    /**
-     * The TextField where the username has to be entered
-     */
-    private static TextField usernameField;
-    /**
-     * The stage of the window
-     */
+    // The stage of the window
     private static final Stage stage = new Stage();
+
+    // the size of the window
+    private static final int WINDOW_WIDTH = 300;
+    private static final int WINDOW_HEIGHT = 200;
+
+    // the vertical distance between the elements
+    private static final int V_SPACE = 10;
+
+    // colours for the styling
+    private static final String BACKGROUND_COLOUR = "#8AA8A1";
+    private static final String BUTTON_COLOUR = "#EE7B30";
+    private static final String FONT_COLOUR = "#FFFFFF";
+
+    // The path to the file where the username is saved
+    private static final String PATH = "src\\main\\resources\\username.txt";
+
+    // The TextField where the username has to be entered
+    private static TextField usernameField;
 
     /**
      * Method to get the saved username
@@ -56,10 +70,15 @@ public class Username {
         VBox root = new VBox(10);
         root.getChildren().addAll(createElements());
         root.setOnMouseClicked(event -> root.requestFocus());
+        root.setAlignment(Pos.TOP_CENTER);
+        root.setPadding(new Insets(V_SPACE, 0, 0, 0));
 
-        Scene scene = new Scene(root, 300, 200);
+        root.setStyle("-fx-background-color: " + BACKGROUND_COLOUR + ";");
+
+        Scene scene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
         stage.setScene(scene);
         stage.showAndWait();
+        stage.setResizable(false);
 
         root.requestFocus();
     }
@@ -95,8 +114,13 @@ public class Username {
 
         Button saveButton = new Button("Save");
         saveButton.setOnAction(event -> saveUsername());
+        saveButton.setStyle("-fx-border-color: black; -fx-border-width: 2px; -fx-font-family: '" + GUI.FONT_FAMILY + "'; -fx-background-color: " + BUTTON_COLOUR + "; -fx-text-fill: " + FONT_COLOUR + ";");
 
-        elements.add(new Label("Enter your username:"));
+        Label usernameLabel = new Label("Enter your username:");
+        usernameLabel.setFont(Font.font(GUI.FONT_FAMILY, FontWeight.THIN, WINDOW_WIDTH / 20.0));
+        usernameLabel.setTextFill(Color.web("#FFFFFF"));
+
+        elements.add(usernameLabel);
         elements.add(usernameField);
         elements.add(saveButton);
 
