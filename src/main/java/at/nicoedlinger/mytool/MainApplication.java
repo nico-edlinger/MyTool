604aefe6989a38c72774b448e019ac331acb96d1
nico-edlinger <edlingernico2008@gmail.com>
2026-08-28 11:50:35 +0200

    update file

--- src/main/java/at/nicoedlinger/mytool/MainApplication.java
+++ src/main/java/at/nicoedlinger/mytool/MainApplication.java
@@ -1,21 +1,27 @@
 package at.nicoedlinger.mytool;
 
+import at.nicoedlinger.mytool.ui.GUI;
 import at.nicoedlinger.mytool.username.Username;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
+import static at.nicoedlinger.mytool.ui.GUI.*;
+
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        Pane root = new Pane();
-        Scene scene = new Scene(root, 320, 240);
+        VBox root = new VBox(GUI.V_SPACE);
+        Scene scene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
         stage.setTitle("Main");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
+
+        createElements(root, scene);
 
         if (!Username.usernameExists()) Username.createUsername();
     }
